8f9e30841d0a69861b6856fc73c96c41406a0501
Illia Petukhov <pavukilla@gmail.com>
2022-09-15 16:21:46 +0300

    fix text

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,9 +3,9 @@ import './app-info.css'
 const AppInfo = ({employees, increased}) => {
     return (
         <div className="app-info">
-            <h1>Учет сотрудников в компании паука</h1>
-            <h2>Общее число сотрудников: {employees}</h2>
-            <h2>Премию получат {increased}</h2>
+            <h1>Учет сотрудников в компании.</h1>
+            <h2>Общее число сотрудников: {employees}.</h2>
+            <h2>С премией: {increased}</h2>
         </div>
     );
 }
