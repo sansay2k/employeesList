092c2cb3552e1bd368050b0da9ee2afadb855a47
Illia Petukhov <pavukilla@gmail.com>
2022-09-15 16:17:18 +0300

    new style + fix

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,15 +1,14 @@
-import { Component } from 'react';
 import './app-filter.css'
 
 const AppFilter = (props) => {
     const buttonsData = [
-        {name: 'all', label: "Все сотпудники"},
+        {name: 'all', label: "Все сотрудники"},
         {name: 'rise', label: "Сотрудники на повышение"},
         {name: 'more1000', label: 'З/П больше 1000$'}
     ]
 
     const buttons = buttonsData.map(({name, label}) => {
-        const active = props.filter == name;
+        const active = props.filter === name;
         const clazz = active ? 'btn-light' : 'btn-outline-light'
         return (
             <button className={`btn ${clazz}`}
